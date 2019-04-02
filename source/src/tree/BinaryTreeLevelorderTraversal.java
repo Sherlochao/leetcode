4cd384e87603adbc6d0b8694c845659b9259107d
zhuyingchao <zhuyingchao@weidian.com>
2019-04-02 15:13:28 +0800

    二叉树的层序遍历

---
+++ source/src/tree/BinaryTreeLevelorderTraversal.java
@@ -0,0 +1,75 @@
+package tree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 给定一个二叉树，返回其按层次遍历的节点值。 （即逐层地，从左到右访问所有节点）。
+ * 例如:
+ * 给定二叉树: [3,9,20,null,null,15,7],
+ *       3
+ *      / \
+ *     9  20
+ *       /  \
+ *      15   7
+ * 返回其层次遍历结果：
+ * [
+ *   [3],
+ *   [9,20],
+ *   [15,7]
+ * ]
+ */
+public class BinaryTreeLevelorderTraversal {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (null != root) {
+            Queue<TreeNode> queue = new LinkedList<>();
+            queue.offer(root);
+            while (!queue.isEmpty()) {
+                int len = queue.size();
+                List<Integer> list = new ArrayList<>();
+                while (len > 0) {
+                    root = queue.poll();
+                    list.add(root.val);
+                    if (null != root.left) {
+                        queue.offer(root.left);
+                    }
+                    if (null != root.right) {
+                        queue.offer(root.right);
+                    }
+                    len--;
+                }
+                result.add(list);
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) throws Exception {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(7);
+        root.left.left.left = new TreeNode(8);
+        root.left.left.right = new TreeNode(9);
+
+        List<List<Integer>> result = new BinaryTreeLevelorderTraversal().levelOrder(root);
+        System.out.println(result);
+    }
+}
