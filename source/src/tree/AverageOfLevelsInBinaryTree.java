0fc1f739c0165998105560388d7c1addf7cf0e0f
zhuyingchao <zhuyingchao@weidian.com>
2019-04-10 18:22:26 +0800

    二叉树的层平均值

---
+++ source/src/tree/AverageOfLevelsInBinaryTree.java
@@ -0,0 +1,63 @@
+package tree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 给定一个非空二叉树, 返回一个由每层节点平均值组成的数组.
+ * 示例 1:
+ * 输入:
+ *      3
+ *     / \
+ *    9  20
+ *      /  \
+ *     15   7
+ * 输出: [3, 14.5, 11]
+ * 解释:
+ * 第0层的平均值是 3,  第1层是 14.5, 第2层是 11. 因此返回 [3, 14.5, 11].
+ * 注意：
+ * 节点值的范围在32位有符号整数范围内。
+ */
+public class AverageOfLevelsInBinaryTree {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> result = new ArrayList<>();
+        if (null == root) {
+            return result;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int size, i;
+        Double sum;
+        while (!queue.isEmpty()) {
+            size = queue.size();
+            i = size;
+            sum = 0d;
+            while (i != 0) {
+                root = queue.poll();
+                sum += root.val;
+                if (null != root.left) {
+                    queue.offer(root.left);
+                }
+                if (null != root.right) {
+                    queue.offer(root.right);
+                }
+                i--;
+            }
+            result.add(sum / size);
+        }
+        return result;
+    }
+}
