08b86fd1aab267c3eeec0a1e813a29516256eafa
zhuyingchao <zhuyingchao@weidian.com>
2019-04-12 13:28:53 +0800

    合并两个有序链表

---
+++ source/src/linkedlist/MergeTwoSortedLists.java
@@ -0,0 +1,58 @@
+package linkedlist;
+
+/**
+ * 将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+ * 示例：
+ * 输入：1->2->4, 1->3->4
+ * 输出：1->1->2->3->4->4
+ */
+public class MergeTwoSortedLists {
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (null == l1 && null == l2) {
+            return null;
+        }
+        if (null == l1) {
+            return l2;
+        }
+        if (null == l2) {
+            return l1;
+        }
+        ListNode head;
+        if (l1.val <= l2.val) {
+            head = l1;
+            l1 = l1.next;
+        } else {
+            head = l2;
+            l2 = l2.next;
+        }
+        ListNode cur = head;
+        while (null != l1 && null != l2) {
+            if (l1.val <= l2.val) {
+                cur.next = l1;
+                l1 = l1.next;
+                cur = cur.next;
+            } else {
+                cur.next = l2;
+                l2 = l2.next;
+                cur = cur.next;
+            }
+        }
+        if (null == l1) {
+            cur.next = l2;
+        }
+        if (null == l2) {
+            cur.next = l1;
+        }
+        return head;
+    }
+}
