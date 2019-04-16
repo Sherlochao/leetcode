645ff96a79c5d2361bdf6bdb4fd62788b63fad21
zhuyingchao <657666322@qq.com>
2019-04-16 22:52:13 +0800

    环形链表

---
+++ source/src/linkedlist/LinkedListCycle.java
@@ -0,0 +1,29 @@
+package linkedlist;
+
+/**
+ * 给定一个链表，判断链表中是否有环。
+ */
+public class LinkedListCycle {
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (null != fast && null != fast.next) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
