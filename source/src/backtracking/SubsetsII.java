773eb6bdfa3c475a4fdb90ab1a45e4be3733d196
zhuyingchao <zhuyingchao@weidian.com>
2019-05-09 12:02:56 +0800

    子集II

--- source/src/backtracking/SubsetsII.java
+++ source/src/backtracking/SubsetsII.java
@@ -1,5 +1,9 @@
 package backtracking;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * 给定一个可能包含重复元素的整数数组 nums，返回该数组所有可能的子集（幂集）。
  *
@@ -17,4 +21,27 @@ package backtracking;
  *      []
  *  ]
  */
-public class SubsetsII {}
+public class SubsetsII {
+
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        backtracking(result, nums, 0, new ArrayList<>());
+        return result;
+    }
+
+    private void backtracking(List<List<Integer>> result, int[] nums, int start, List<Integer> cur) {
+        result.add(new ArrayList<>(cur));
+        for (int i = start; i < nums.length; i++) {
+            if (i == start || nums[i] != nums[i - 1]) {
+                cur.add(nums[i]);
+                backtracking(result, nums, i + 1, cur);
+                cur.remove(cur.size() - 1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new SubsetsII().subsetsWithDup(new int[] {1, 2, 2}));
+    }
+}
