40efc3fc1d9c14d75821fe6e7df0bc1decc762ad
zhuyingchao <zhuyingchao@weidian.com>
2019-05-09 13:26:29 +0800

    全排列II

---
+++ source/src/backtracking/PermutationsII.java
@@ -0,0 +1,24 @@
+package backtracking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 给定一个可包含重复数字的序列，返回所有不重复的全排列。
+ *
+ * 示例:
+ * 输入: [1,1,2]
+ * 输出:
+ *  [
+ *      [1,1,2],
+ *      [1,2,1],
+ *      [2,1,1]
+ *  ]
+ */
+public class PermutationsII {
+
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        return result;
+    }
+}
