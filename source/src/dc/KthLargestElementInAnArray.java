c5c241e4cfe830980cfa05098f11b48ed2c12b7e
zhuyingchao <zhuyingchao@weidian.com>
2019-05-14 21:01:10 +0800

    数组中的第K个最大元素

--- source/src/dc/KthLargestElementInAnArray.java
+++ source/src/dc/KthLargestElementInAnArray.java
@@ -1,5 +1,7 @@
 package dc;
 
+import java.util.Arrays;
+
 /**
  * 在未排序的数组中找到第 k 个最大的元素。请注意，你需要找的是数组排序后的第 k 个最大的元素，而不是第 k 个不同的元素。
  *
@@ -17,6 +19,7 @@ package dc;
 public class KthLargestElementInAnArray {
 
     public int findKthLargest(int[] nums, int k) {
-        return 0;
+        Arrays.sort(nums);
+        return nums[nums.length - k];
     }
 }
