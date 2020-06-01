5426a9dfad5d39f3b23a7ed4281d1b1edf450210
朱颖超 <zhuyingchao@maoyan.com>
2020-06-01 15:01:20 +0800

    base TreeNode

---
+++ source/src/base/TreeNode.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2017 maoyan.com
+ * All rights reserved.
+ *
+ */
+package base;
+
+/**
+ * 在这里编写类的功能描述
+ *
+ * @author zhuyingchao
+ * @created 2020-06-01
+ */
+public class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+
+}
